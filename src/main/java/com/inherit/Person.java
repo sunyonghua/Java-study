d9938b0b2af3793fd477acb80d26c5be0e31a7ec
sunyonghua <yonghuasir@icloud.com>
2019-04-16 09:50:26 +0800

    add: overload and override

--- src/main/java/com/inherit/Person.java
+++ src/main/java/com/inherit/Person.java
@@ -2,13 +2,9 @@ package com.inherit;
 
 /**
  * 子类拥有父类非 private 的属性、方法。
- * <p>
  * 子类可以拥有自己的属性和方法，即子类可以对父类进行扩展。
- * <p>
  * 子类可以用自己的方式实现父类的方法。
- * <p>
  * Java 的继承是单继承，但是可以多重继承，单继承就是一个子类只能继承一个父类，多重继承就是，例如 A 类继承 B 类，B 类继承 C 类，所以按照关系就是 C 类是 B 类的父类，B 类是 A 类的父类，这是 Java 继承区别于 C++ 继承的一个特性。
- * <p>
  * 提高了类之间的耦合性（继承的缺点，耦合度高就会造成代码之间的联系越紧密，代码独立性越差）。
  */
 public class Person {
@@ -22,6 +18,11 @@ public class Person {
 
   public void eat() {
     System.out.println(name + "在吃饭");
+    this.meal();
+  }
+
+  public void meal() {
+    System.out.println("吃火锅");
   }
 
   public void play() {
